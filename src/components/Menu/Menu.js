3739a08307fe41de06457f1cc8c8dd8ec994be26
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-19 14:56:18 -0700

    Add filtering of buttons to App and prop drill it down

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Menu.css'
 
-const Menu = () => {
-
+const Menu = ({ allNews, filterNews }) => {
     return (
         <section>
-            <button>Local News</button>
-            <button>Technology</button>
-            <button>Entertainment</button>
-            <button>Science</button>
-            <button>Health</button>
+            <button onClick={() => filterNews(allNews.local)} >Local News</button>
+            <button onClick={() => filterNews(allNews.technology)} >Technology</button>
+            <button onClick={() => filterNews(allNews.entertainment)} >Entertainment</button>
+            <button onClick={() => filterNews(allNews.science)} >Science</button>
+            <button onClick={() => filterNews(allNews.health)} >Health</button>
         </section>
     )
 }
