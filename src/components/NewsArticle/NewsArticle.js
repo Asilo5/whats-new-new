76305fcb0d9a01727b80214c1693e7044b7b04a8
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-19 18:55:24 -0700

    Add style to url link to continue reading article

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './NewsArticle.css';
 
-const NewsArticle = ({ headline, img, description }) => {
+const NewsArticle = ({ headline, img, description, url }) => {
     return (
         <section className='news-article'>
             <img src={img} alt={headline} />
             <h2>{headline}</h2>
-             <h3>{description}</h3>
+            <h3>{description}</h3>
+            <a href={url}>Continue Reading Article</a>
         </section>
     )
 }
