e05d998c033888e9709abb3bf764892aa91518db
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-11-22 19:00:35 -0700

    Updates

---
+++ precache-manifest.4392e787125e17dbdedbaad1270a330b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb7e7490042a08c567c5c24138d3b840",
+    "url": "/whats-new-times/index.html"
+  },
+  {
+    "revision": "714c360f09919f1c0483",
+    "url": "/whats-new-times/static/css/main.abdec34f.chunk.css"
+  },
+  {
+    "revision": "90e8e4f362ac0c6c99a9",
+    "url": "/whats-new-times/static/js/2.534a1664.chunk.js"
+  },
+  {
+    "revision": "714c360f09919f1c0483",
+    "url": "/whats-new-times/static/js/main.cbfb968d.chunk.js"
+  },
+  {
+    "revision": "88ab3074f7d283c6964e",
+    "url": "/whats-new-times/static/js/runtime-main.fe32ea26.js"
+  }
+]);
